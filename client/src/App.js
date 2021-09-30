eab6926f8148467b7f75511baa19e5dce99b6bd0
Kayla Cormier <kaylacormier@Kaylas-MacBook-Pro.local>
2021-09-30 16:02:52 -0500

    Some front end, but addPlant is not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,100 @@
-import logo from './logo.svg';
+import { response } from 'express';
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  let [plant, setPlants] = useState([]);
+  let [plants, setPlants] = useState([]);
+  const formInitialState = {plantName: "", username: ""};
+  let [formData, setFormData] = useState([formInitialState]);
 
-const addPlant = async (plantName) => {
-  let options = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(plant),
+  const getPlants = () => {
+    fetch('/:plants')
+      .then((res) => res.json())
+      .then((plants) => {
+         setPlants(plants);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
-  
-  try {
-    let response = await fetch("/plants", options);
-    if (response.ok) {
-      let plants = await response.json();
-      setPlants(plant);
-    } else {
-      console.log("server error:", response.status, response.statusText);
-    }
-  } catch (err) {
-    console.log("Network error:", err.message);
+
+useEffect(()=> {
+  getPlants();
+}, []);
+
+  const handleInputChange = (event) => {
+    // let newId = plants.length;
+    //newPlant.id = newId;
+    let { name, value /*,username, value2*/ } = event.target;
+    setFormData({...formData, [name]: value, /*[username]: value2*/});
   }
-} 
+    // add newPlant to State
+    // setPlants((state) => [...state, newPlant]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    addPlant(formData.plant/*, formData.username*/);
+    setPlants(formInitialState);
+  };
+
+//   const addPlant = async (name/*, username*/) => {
+//     let plant = { name/*, username*/ };
+//     let options = {
+//       method: "POST",
+//       headers: {"Content-Type": "application/json" },
+//       body: JSON.stringify(plant),
+//     };
+
+//     try {
+//       let response = await fetch("/plants", options);
+//       if (response.ok) {
+//         let plants = await response.json();
+//         setPlants(plants);
+//       }
+//     } catch (err) {
+//       console.log("Network error:", err);
+//     }
+// };
+
+//   const addPlant = async (name) => {
+//     let plant = { name/*, username*/ };
+//     let options = {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify(plant),
+//     };
+
+//     try {
+//       await fetch("/students", options);
+//       getPlants();
+//     } catch (err) {
+//       console.log("Network error:", err);
+//     }
+// } ;
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h3> Build Your Garden </h3><header className="button-container"> 
+      <form>
+       <label className="plantadd">New Plant</label>
+       <input
+       type= "text"
+       onChange={(e) => handleInputChange(e)}
+       name="plantadder"
+       value={ formData.plant }
+       placeholder="Your plant here"
+       />
+
+        <button
+        className="button" 
+        > Add Plant
+        </button>
+        </form>
+        
+        
+    </header>  
+    
     </div>
   );
 }
