2ddfc54d1cc47eae474747a3cb20eaeb4507a2c5
Kayla Cormier <kaylacormier@Kaylas-MacBook-Pro.local>
2021-10-06 23:28:07 -0500

    Add Images?

--- client/src/App.js
+++ client/src/App.js
@@ -17,10 +17,6 @@ function App() {
       });
   };
 
-  useEffect(()=> {
-    getPlants();
-}, []);
-
   function handleInputChange(event) {
     let { name, value } = event.target;
     setFormData({...formData, [name]: value});
@@ -53,11 +49,42 @@ function App() {
     }
 };
 
+useEffect(()=> {
+  getPlants();
+}, []);
 
   return (
-    <div className="title p-3 mb-2 bg-success text-white">
+    <div className="bg-dark">
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Watering Reminders
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Text Message Alerts</a></li>
+            <li><a class="dropdown-item" href="#">Email Alerts</a></li>
+            <li><a class="dropdown-item" href="#">Stop Alerts</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+      <div className="title p-3 mb-2 bg-success text-white">
     <h1 className="title"> Feed Me, Seymore </h1>
+    </div>
 
     {/* <header className="button-container" > Add Plants Here: </header> */}
       <form className="grid-container" onSubmit={handleSubmit}>
@@ -72,20 +99,28 @@ function App() {
        
        <label className="usernameadd">Username</label>
        <input
+       type= "image"
+       onChange={e => handleInputChange(e)}
+       name="plantimage"
+       value={ formData.image }
+       placeholder="Username here"
+       /> 
+
+      <label className="pictureadd">Username</label>
+       <input
        type= "text"
        onChange={e => handleInputChange(e)}
        name="username"
        value={ formData.username }
        placeholder="Username here"
-       />
+       /> 
 
         <button className="submit" 
         > Add Plant
         </button>
         </form>
        
-          <h3>{plants.plantName}</h3>       
-        <div className="row row-cols-sm-3 row-cols-md-3">
+        <div className="row row-cols-sm-3">
         {plants.map((plants) => (
           <div className= "col-sm">
             <div className="col">
@@ -96,26 +131,22 @@ function App() {
               </div>
             <div className="card">
               <div className="card-body shadow-border-0">
-                <h4> {plants.plantName}</h4>
+                </div>
+                <h4> { plants.plantName }</h4>
                  <div className="header">
-                 Last Watered: {plants.lastWatered}
+                 Last Watered: { plants.lastWatered }
+                 <img
+                  src={""}
+                  class="card-img-top"
+                   alt="..."
+                    />
+                 </div>
                  <div className="card-header shadow-border-0">
-                </div>
-                </div>
-                </div>
-                </div>
-                </div>
-              
-                
-                
-       
+                  </div>
+              </div>
+            </div>  
         ))}
 
-        
-
-
-
-        
      </div>
      </div>
    
